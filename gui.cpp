e828673eef27fce3ab11ab522451bc9f35a7a3a3
Torbjörn Rathsman <milasudril@gmail.com>
2014-04-19 14:10:02 +0200

    ...

---
+++ gui.cpp
@@ -0,0 +1,44 @@
+#ifdef __WAND__
+
+target[
+	name[
+		gui.o
+	]
+	type[
+		object
+	]
+	platform[;Windows]
+]
+#endif
+
+#include "gui.h"
+#include <windows.h>
+
+int Gui::Gui::run()
+	{
+	MSG message;
+
+	while(window_count)
+		{
+		if(GetMessage(&message, NULL, 0, 0) > 0)
+			{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+			}
+		else
+			{return message.wParam;}
+		}
+	return 0;
+	}
+
+Gui::Gui::~Gui()
+	{
+	}
+	
+void Gui::Gui::destroy()
+	{
+	delete this;
+	}
+
+
+	
